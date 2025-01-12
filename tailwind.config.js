d9b99635bd61ddf85757f97036a015399b907f3b
JuanCastro <jcastro@colegium.com>
2025-01-12 19:13:55 -0300

    [add] diseño resposivo con tailwind mas medidas de tailwind para poder agregar propias

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens:{
+        'tablet': '900px',
+      },
       colors:{
         'azul-claro' : ' #243cff ' ,
         'azul-oscuro': ' #0d1b3e ',
